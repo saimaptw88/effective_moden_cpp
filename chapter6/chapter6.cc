a9991686e7371c2e3f4d1faca15df4756ff1f012
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2023-12-16 14:35:52 +0900

    add 31

---
+++ chapter6/chapter6.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "chapter6.hh"
+
+
+namespace chapter6 {
+void execute() {
+  std::cout << "***** CHAPTER6 *****" << std::endl;
+}
+};
--- main.cc
+++ main.cc
@@ -1,10 +1,13 @@
 #include <iostream>
 
 #include "chapter5/chapter5.hh"
+#include "chapter6/chapter6.hh"
 
 
 int main() {
   std::cout << "hello world" << std::endl;
   chapter5::execute();
+  chapter6::execute();
+
   return 0;
 }

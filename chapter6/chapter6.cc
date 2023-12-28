8732d8e226b6a89a2ad1b730c55404dea372fbcc
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2023-12-28 14:14:36 +0900

    add section 32

--- chapter6/chapter6.cc
+++ chapter6/chapter6.cc
@@ -1,10 +1,15 @@
 #include <iostream>
 
 #include "chapter6.hh"
+#include "chapter6_31.hh"
+#include "chapter6_32.hh"
 
 
 namespace chapter6 {
 void execute() {
   std::cout << "***** CHAPTER6 *****" << std::endl;
+
+  _31::execute();
+  _32::execute();
 }
 };

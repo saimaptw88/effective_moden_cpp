c085d16d556858ad66560f64a9cab8eb150c229c
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2024-01-28 13:55:19 +0900

    add future functions

--- chapter7/section38.hh
+++ chapter7/section38.hh
@@ -1,8 +1,13 @@
 #ifndef SECTION38_HH_
 #define SECTION38_HH_
 
+#include <future>
+
+
 namespace chapter7 {
 namespace section38 {
+void calc(int start, std::promise<int> p);
+void client();
 void execute();
 }; // namespace section38
 };  // namespace chapter7

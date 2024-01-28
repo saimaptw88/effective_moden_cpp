0059a7e5d384aaaaaf0da31e6bdf1db61960b350
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2024-01-28 23:27:43 +0900

    add section41

--- chapter8/chapter8.cc
+++ chapter8/chapter8.cc
@@ -1,13 +1,13 @@
 #include <iostream>
 
 #include "chapter8.hh"
-#include "section40.hh"
+#include "section41.hh"
 
 
 namespace chapter8 {
 void execute() {
   std::cout << "CHAPTER 8" << std::endl;
 
-  section40::execute();
+  section41::execute();
 }
 };  // namespace chapter8
---
+++ chapter8/section41.hh
@@ -0,0 +1,11 @@
+#ifndef SECTUION41_HH_
+#define SECTUION41_HH_
+
+
+namespace chapter8 {
+namespace section41 {
+void execute();
+};  // namespace section41
+};  // namespace chapter8
+
+#endif   // SECTUION41_HH_

bdfb65f3921b7c2ef9284f9a3f4110dc75a63be5
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2024-02-03 10:23:42 +0900

    add files

--- chapter8/chapter8.cc
+++ chapter8/chapter8.cc
@@ -2,6 +2,7 @@
 
 #include "chapter8.hh"
 #include "section41.hh"
+#include "section42.hh"
 
 
 namespace chapter8 {
@@ -9,5 +10,6 @@ void execute() {
   std::cout << "CHAPTER 8" << std::endl;
 
   section41::execute();
+  section42::execute();
 }
 };  // namespace chapter8
---
+++ chapter8/section42.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "section42.hh"
+
+
+namespace chapter8 {
+namespace section42 {
+void execute() {
+  std::cout << "SECTUION 42" << std::endl;
+}
+};  // namespace section42
+};  // namespace chapter8

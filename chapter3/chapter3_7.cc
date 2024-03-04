a66a9d2930194c5abcd774f2b18d6d97259607ea
斎藤正大 <masahiro@saitoumasahironoMacBook-Pro.local>
2024-03-04 23:32:06 +0900

    add section7

--- chapter3/chapter3_7.cc
+++ chapter3/chapter3_7.cc
@@ -4,6 +4,19 @@
 
 
 namespace chapter3 {
+class Widget {
+  public:
+    Widget(int i, bool b);
+    Widget(int i, double d);
+    Widget(std::initializer_list<long double> il);
+
+    operator float() const;
+
+  private:
+    int i_;
+    bool b_;
+    double d;
+};
 namespace section7 {
 void executive() {
   std::cout << "SECTION 7" << std::endl;
